03533b9d11061b26dd0952b5ac749632a6c56aca
Majd-Bohsali <118876575+Majd-Bohsali@users.noreply.github.com>
2025-09-19 17:15:42 -0700

    Created struct

---
+++ 210-lab-11.cpp
@@ -0,0 +1,15 @@
+// COMSC 210 | Lab 11 | Majd Bohsali
+// IDE used: Visual Studio Code 
+#include <iostream>
+using namespace std; 
+
+struct Student {
+    string name;
+    int studentID; 
+    int graduationYear; 
+    double* grades; 
+};
+
+int main() {
+    return 0; 
+}
